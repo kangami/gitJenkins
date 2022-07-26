b75363f2a0fb9e943c4f9cd0f6fcb6dfe8d4020e
Karim <ngamikarim13@gmail.com>
2022-07-26 02:21:16 -0400

    complexefunction have been modified

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def myJenkinsFunction():
     print("Here is our first function")
 
 def complexefunction():
-    print("I am karim and i am a successful guy who always get what he want")
+    print("I am karim and i am a successful guy who always get what he want in life")
 
 complexefunction()
 myJenkinsFunction()
